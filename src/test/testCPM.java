b2d651d38a018308fd2bd2a4009ab81958d72920
Julian Grijalba <caprijuda@gmail.com>
2017-05-29 02:45:32 -0500

    Homework example added to test

--- src/test/testCPM.java
+++ src/test/testCPM.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package test;
 
 import java.util.ArrayList;
@@ -14,10 +9,6 @@ import models.Actividad;
 import models.CPM;
 import models.Node;
 
-/**
- *
- * @author Yuliamz
- */
 public class testCPM {
  
     public static void main(String[] args) {
